28cf5f1db434afb5911df1f52cee562361dec31b
BelchieBen <76218159+BelchieBen@users.noreply.github.com>
2021-11-18 22:32:42 +0000

    added an allowed host

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-enf#_vg^&rcd@5uml2x-!slxxv=%f6u&+az8^$w!afntn=c2df
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['bens-portfolio-web.herokuapp.com']
 
 
 # Application definition
